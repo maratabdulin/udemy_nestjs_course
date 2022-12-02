9ab5301e65bc9885963f52dfd7a470b8a01a627a
Marat Abdulin <abdulinmm@gmail.com>
2022-12-02 16:41:21 +0300

    2 add models in modules

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // все запросы должны иметь api в url
   await app.listen(3000);
 }
 bootstrap();
